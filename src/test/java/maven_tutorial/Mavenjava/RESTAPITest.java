da9618655785c8b4b4a79dff03566c04694bafc5
ChrisBGraphics <cboro007@gmail.com>
2020-07-22 13:56:07 -0600

    First commit

---
+++ src/test/java/maven_tutorial/Mavenjava/RESTAPITest.java
@@ -0,0 +1,19 @@
+package maven_tutorial.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("JIRA");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("Twitter is gone");
+	}
+
+}
---
+++ src/test/java/maven_tutorial/Mavenjava/SeleniumTest.java
@@ -0,0 +1,20 @@
+package maven_tutorial.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Internet");
+	}
+	
+	@Test
+	public void elements()
+	{
+		System.out.println("Internet");
+	}
+	
+	
+}
